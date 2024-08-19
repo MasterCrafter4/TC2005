61434e7292b31497673fd2eeab0d979dfbee119a
MasterCrafter69 <r2d2.dany@gmail.com>
2024-08-18 23:36:46 -0600

    finalizar lab6 y lab8

---
+++ js/poe.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById('boton_chilaquiles');
+
+boton.onclick = () => {
+    const imagen = document.getElementById('imagen_chilaquiles');
+    imagen.alt = "Imagen de plato con totopos";
+}
