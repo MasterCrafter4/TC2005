dc129a89393009808dfa39c0ec95f254907dcd92
MasterCrafter69 <r2d2.dany@gmail.com>
2024-08-20 12:24:50 -0600

    agregar middleware

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("adios")
+
+const express = require('express');
+const app = express();
+
+//Middleware algo que hay en medio entre la petición y la respuesta 
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);
